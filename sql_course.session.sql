c0bbe1a45748e0d001a7d54e1c1891918cd16fca
Eddie Jimenez <eddiemjimenez3@gmail.com>
2025-02-14 16:28:45 -0500

    push example

---
+++ sql_course.session.sql
@@ -0,0 +1,13 @@
+eddiejimenez@Eddies-MacBook-Air SQL_project_data_job_analysis % git config -- global user.name "Eddie Jimenez"                               
+git config --global user.email "eddiemjimenez3@gmail.com" 
+
+
+
+git config --global user.name "Eddie Jimenez"
+git config --global user.email "eddiemjimenez3@gmail.com"
+
+
+
+git remote add origin https://github.com/eddiej3/SQL_project_data_job_analysis.git
+git branch -M main
+git push -u origin main
